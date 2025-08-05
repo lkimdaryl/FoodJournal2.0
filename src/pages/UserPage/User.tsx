3ed33befda3d655a16bacdcec42c8c353dcaf798
lkimdaryl <lkimdaryl@gmail.com>
2025-08-05 11:17:31 -0700

    userpage update

--- src/pages/UserPage/User.tsx
+++ src/pages/UserPage/User.tsx
@@ -4,6 +4,7 @@ import PostList from '../../components/postlists/postlist.tsx'
 import PostCard from '../../components/postcard/postcard';
 import rawPosts from '../../database/posts.json';
 import Cookies from 'js-cookie';
+import DefaultPic from '/blankProfile.png?url'
 
 type Post = {
   food_name: string;
@@ -24,7 +25,8 @@ export default function UserPage() {
     const location = useLocation();
     const user = Cookies.get('username'); // for demonstration purposes, using cookie to get username
     const posts = rawPosts as PostsByUser;
-    const { username, userId, profilePic } = location.state || {};   
+    const { username, userId, profilePic } = location.state || {};     
+    
     const currentUserId = Cookies.get('userId');
     const baseUrl = import.meta.env.VITE_APP_BASE_URL;
     const fetchUrl = userId ? `${baseUrl}/api/v1/post_review/get_posts_by_id?post_id=${userId}` : undefined;
@@ -32,9 +34,8 @@ export default function UserPage() {
     return (
         <div className='post-container'>
             <div id='profile-section'>
-                <img id='profile-pic' src="https://placehold.co/100" alt="Profile" />
+                <img id='profile-pic' src={profilePic || DefaultPic} alt="Profile" />
                 <span>{user}</span>
-                {/* <span>{userPosts[0].username}</span> */}
             </div>
             {user && posts[user]?.map((post: Post, index: number) => (
                 <PostCard
@@ -42,7 +43,7 @@ export default function UserPage() {
                     id={index}
                     user_id={index}
                     username={post.username}
-                    profile_pic="https://placehold.co/100"
+                    profile_pic={post.profile_pic || DefaultPic}
                     food_name={post.food_name}
                     rating={post.rating}
                     review={post.review}
