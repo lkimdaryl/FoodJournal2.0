5726ec07d912bd2cc9118f4c148d90a79b5345f2
lkimdaryl <lkimdaryl@gmail.com>
2025-08-09 15:25:27 -0700

    ui changes

--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -5,7 +5,7 @@ import Cookies from 'js-cookie';
 export default function Login() {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const usernameInputRef = useRef<HTMLInputElement>(null);
   const baseUrl = import.meta.env.VITE_APP_BASE_URL;
@@ -18,7 +18,7 @@ export default function Login() {
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     setLoading(true);
-    setError(null);
+    setError('');
 
     // Simulate network delay
     await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -31,7 +31,6 @@ export default function Login() {
       window.location.href = "/demouserpage";
     } else {
       try {
-
         const formData = new URLSearchParams();
         formData.append('username', username);
         formData.append('password', password);
@@ -45,18 +44,20 @@ export default function Login() {
           body: formData.toString(),
         });
         
-        const data = await response.json();
-        console.log(data);
-        Cookies.set('signedIn', 'true');
-        Cookies.set('user', username);
-        Cookies.set('accessToken', data.access_token);
-        Cookies.set('userId', data.user_id);
-        Cookies.set('profilePicture', data.profile_picture);
-        window.location.href = "/mypage";
-
+        if (response.ok){
+          const data = await response.json();
+          console.log(data);
+          Cookies.set('signedIn', 'true');
+          Cookies.set('user', username);
+          Cookies.set('accessToken', data.access_token);
+          Cookies.set('userId', data.user_id);
+          Cookies.set('profilePicture', data.profile_picture);
+          window.location.href = "/mypage";
+        } else {
+          setError('Invalid username or password.');
+        }
       } catch (error) {
         console.error('Login failed:', error);
-        setError('Invalid username or password.');
       }
     }
 
@@ -85,7 +86,7 @@ export default function Login() {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        {error && <div className="error-message">{error}</div>}
+        <div className="error-message">{error}</div>
         <a className="forgot" href="https://youtu.be/b3rNUhDqciM">Forgot Password?</a>
         <button className="submit" disabled={loading}>{loading ? 'Loggin in...' : 'Login'}</button>
         <p className='notice'>New to Food Journal?</p>
